011491847108a990972611f3de02eb828bac8ad6
Remco de Man <github@remcodeman.nl>
2014-01-21 11:02:52 +0100

    Test voor commiting to GitHub

--- dev/woud.php
+++ dev/woud.php
@@ -3,6 +3,8 @@
 <?php
 include "config/config.inc.php";
 
+print_r("Test");
+
 User::register($db,"johan.user","wachtwoord2","wachtwoord2","johan","jansen","432","","johan@hotmail.com");
 ?>
 </head>
