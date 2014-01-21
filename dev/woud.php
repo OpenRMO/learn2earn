47f1e577b9f12648ea4410c3effadef9bcd537b9
Remco de Man <github@remcodeman.nl>
2014-01-21 11:16:49 +0100

    Code terugveranderd naar oorspronkelijke staat.

--- dev/woud.php
+++ dev/woud.php
@@ -3,8 +3,6 @@
 <?php
 include "config/config.inc.php";
 
-print_r("Test");
-
 User::register($db,"johan.user","wachtwoord2","wachtwoord2","johan","jansen","432","","johan@hotmail.com");
 ?>
 </head>
