8d585940d522f0cf368cfe4751ddfedd37bdb1ec
Roy <Roy@Roy-laptop>
2014-02-04 18:49:28 +0100

    '$error' veranderd in $error

--- layout/login.php
+++ layout/login.php
@@ -4,13 +4,13 @@ $password = $_POST["password"];
 
 include "../config/config.inc.php";
 include "../classes/user.class.php";
-$error = User.login();
+$error = User::login($db, $username, $password);
 if($error === 1)
 {
     header("Location: index.php");
 }
 else
 {
-    header("Location: index.php?error='$error'");
+    header("Location: index.php?error=$error");
 }
 ?>
