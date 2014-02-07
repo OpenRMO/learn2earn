562968297ed586c58d6b630e1a6cdb3cd2721b4a
Roy <Roy@10.41.14.1>
2014-02-07 11:15:39 +0100

    register_next.php toegevoegd

--- layout/register_next.php
+++ layout/register_next.php
@@ -1,3 +1,14 @@
 <?php
+
+include("../config/config.inc.php");
 $username = $_POST["username"];
+$pass1 = $_POST["password1"];
+$pass2 = $_POST["password2"];
+$fn = $_POST["firstname"];
+$ln = $_POST["lastname"];
+$studentNumber = $_POST["studentnumber"];
+$birthDate = $_POST["birthdate"];
+$email = $_POST["email"];
+
+User::register($db, $username, $pass1, $pass2, $fn, $ln, $studentNumber, $birthdate, $email);
 ?>
