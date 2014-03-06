a5d81b044e0ba09d270f73e7e501d7dec5b6d7a6
Leanderizzle <leander@vanb33k.nl>
2014-03-07 00:47:40 +0100

    Errorhandling geupdated

--- layout/add_course.php
+++ layout/add_course.php
@@ -1,15 +1,15 @@
 <?php
 if(!isset($_POST['project']) || !isset($_POST['beschrijving'])){
-    die(header("Location: register.php?error=2"));
+    die(header("Location: register.php?error=11"));
 }
 
 include("../config/config.inc.php");
-is_int($_POST["project"]) ? header("Location: register.php?error=2") : $project = ($_POST["project"]);
+is_int($_POST["project"]) ? header("Location: register.php?error=11") : $project = ($_POST["project"]);
 $description = $db->escape($_POST["beschrijving"]);
 isset($_FILES) ? $file = $_FILES["filename"] : $file=null;
 isset($_POST["youtube"]) ? $youtube_link = $db->escape($_post["youtube"]) : $youtube_link = null;
 
-$filename = file::uploadFile($file) != false ? $error = null : header("Location: register.php?error=5");
+$filename = file::uploadFile($file) != false ? $error = null : header("Location: register.php?error=15");
 
 $error = course::add($db, $project, $description, $file, $youtube_link);
 header("Location: register.php?error=1");
